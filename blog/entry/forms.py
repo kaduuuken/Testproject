709d669e4e3965363f8811443065398e1fd7a1ab
Kathrin Wende <kathrin.wende@redmoonstudios.de>
2012-05-23 11:39:49 +0200

    changed from Forms to ModelForms

--- blog/entry/forms.py
+++ blog/entry/forms.py
@@ -1,6 +1,11 @@
 from django import forms
+from django.forms import ModelForm
+from entry.models import Post
 
-class PostForm(forms.Form):
+class PostForm(ModelForm):
 	title = forms.CharField(error_messages={'required': 'Bitte einen Titel eingeben'})
-	content = forms.CharField(min_length=5, widget=forms.Textarea, error_messages={'required': 'Bitte einen Inhalt eingeben','min_length':'Bitte mindestens 5 Zeichen eingeben'})
+	content = forms.CharField(min_length=5, widget=forms.Textarea, 
+		  error_messages={'required': 'Bitte einen Inhalt eingeben','min_length':'Bitte mindestens 5 Zeichen eingeben'})
+	class Meta:
+		model = Post
 
