053797fcc6c6f07baf37f095fdd571141e194dcc
Kathrin Wende <kathrin.wende@redmoonstudios.de>
2012-05-25 11:44:23 +0200

    add new urls

--- blog/entry/urls.py
+++ blog/entry/urls.py
@@ -3,7 +3,8 @@ from django.views.generic import DetailView, ListView, CreateView
 from entry.models import Post,Comment
 
 urlpatterns = patterns('entry.views',
-    url(r'^$', 'test_view'),
-    #url(r'^(?P<pk>\d+)/$', 'detail'),
-    #url(r'^add/$', 'new'),
+    url(r'^$', 'index_view', name='home'),
+    url(r'^add/$', 'test_view'),
+    url(r'^(?P<post_id>\d+)/$', 'detail_view', name="entry"),
+    url(r'^(?P<post_id>\d+)/add/$', 'comment_view'),
 )
