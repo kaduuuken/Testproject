fa787d19789be0c73ebedacd1c872a7f9a15359f
Kathrin Wende <kathrin.wende@redmoonstudios.de>
2012-05-25 11:39:24 +0200

    add CommentForm

--- blog/entry/forms.py
+++ blog/entry/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.forms import ModelForm
-from entry.models import Post
+from entry.models import Post, Comment
 
 class PostForm(ModelForm):
 	title = forms.CharField(error_messages={'required': 'Bitte einen Titel eingeben'})
@@ -8,4 +8,14 @@ class PostForm(ModelForm):
 		  error_messages={'required': 'Bitte einen Inhalt eingeben','min_length':'Bitte mindestens 5 Zeichen eingeben'})
 	class Meta:
 		model = Post
+		exclude = ('user')
+
+class CommentForm(ModelForm):
+	name = forms.CharField(error_messages={'required': 'Bitte einen Namen eingeben'})
+	text = forms.CharField(min_length=5, widget=forms.Textarea, 
+		  error_messages={'required': 'Bitte einen Inhalt eingeben','min_length':'Bitte mindestens 5 Zeichen eingeben'})
+	email = forms.EmailField(error_messages={'required': 'Bitte eine Email-Adresse eingeben', 'invalid': 'Bitte eine korrekte Email-Adresse eingeben'})
+	post = forms.ModelChoiceField(queryset=Post.objects.all(), widget=forms.HiddenInput)
+	class Meta:
+		model = Comment
 
