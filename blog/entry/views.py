39e68a7d980d91bdbff07c396533fc75fa89474f
Kathrin Wende <kathrin.wende@redmoonstudios.de>
2012-05-25 11:44:44 +0200

    add new views

--- blog/entry/views.py
+++ blog/entry/views.py
@@ -1,20 +1,48 @@
-from django.shortcuts import get_object_or_404, render_to_response
+from django.shortcuts import get_object_or_404, render_to_response, redirect
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 from django.template import RequestContext
-from entry.models import Post
-from entry.forms import PostForm
+from entry.models import Post, Comment
+from entry.forms import PostForm, CommentForm
+
+def index_view(request):
+	p = Post.objects.all().order_by('-pub_date')
+    	return render_to_response('entry/index.html', {'p': p})
+
+
+def detail_view(request, post_id):
+    	p = get_object_or_404(Post, pk=post_id)
+    	return render_to_response('entry/detail.html', {'post': p})
+
+
+def comment_view(request, post_id):
+	p = get_object_or_404(Post, pk=post_id)
+	if request.method == 'POST':
+		form = CommentForm(request.POST, initial={'post': p})
+		if form.is_valid():
+			new_comment = form.save()
+			render_to_response('entry/comment.html', {'form': form})
+			return redirect('entry', post_id)
+		else:
+			return render_to_response('entry/comment.html', {'form': form})
+	else:
+		form = CommentForm(initial={'post': p})
+	return render_to_response('entry/comment.html', {'form': form,}) 
+
 
 def test_view(request):
 	if request.method == 'POST':
 		form = PostForm(request.POST)
 		if form.is_valid():
+			new_post = form.save()
 			if request.user.is_authenticated():
+				new_post.user = request.user
 				username = request.user
+				new_post.save()
 			else:
 				username = 'Gast'
-			new_post = form.save()
-			return render_to_response('entry/test.html', {'form': form, 'username':username})
+			render_to_response('entry/test.html', {'form': form, 'username':username})
+			return redirect('home')
 		else:
 			return render_to_response('entry/test.html', {'form': form})
 	else:
